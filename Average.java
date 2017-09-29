5d74f8cbb67731c4cb21d017cf5fb67548f62ca0
GuillaumeDgr <dangerguillaume@gmail.com>
2017-09-29 10:24:02 +0200

    commit

--- Average.java
+++ Average.java
@@ -3,12 +3,9 @@ class Average {
 	public static float average(int[] values) {
 		float total = 0;
 
-		// début de ton code ici !
-
-
-
-		// fin de ton code :)
-
+		for (int i = 0; i < values.length; i++) {
+			total = values[i] + total;
+		}
 		return total / values.length;
 	}
 }
